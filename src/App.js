96297880994330c033ca185b734e63449a5651e8
Marcos Fellipe <marcosdeda93@gmail.com>
2020-01-08 18:57:59 -0300

    Adicionado tamanho, pão e sabor. Removido css do react.

--- src/App.js
+++ src/App.js
@@ -4,21 +4,30 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Tamanho:</h2>
+      <button>15cm</button>
+      <button>30cm</button>
+      <h2>Pão:</h2>
+      <button>9 grãos</button>
+      <button>9 grãos com aveia e mel</button>
+      <button>Italiano</button>
+      <button>Três queijos</button>
+      <button>Parmesão e orégano</button>
+      <h2>Sabor:</h2>
+      <button>Atum</button>
+      <button>Beef Bacon Chiplote</button>
+      <button>Beef Barbecue Bacon</button>
+      <button>B.M.TCopyright Italiano</button>
+      <button>Frango</button>
+      <button>Frango defumado com Cream Cheese</button>
+      <button>Frango Empanado</button>
+      <button>Frango Pesto Cream Cheese</button>
+      <button>Frango Ranch</button>
+      <button>Frango Teriyaki</button>
+      <button>Steak Cheddar Cremoso</button>
+      <button>Steak Churrasco</button>
+      <button>Vegetariano</button>
     </div>
   );
 }
